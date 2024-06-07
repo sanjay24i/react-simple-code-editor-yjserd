d72f4926a2a563954166b2f7bb2520e985e9d685
sanjay24i <135849018+sanjay24i@users.noreply.github.com>
2024-06-07 13:58:48 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-simple-code-editor-yjserd
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/sanjay24i/react-simple-code-editor-yjserd)
---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+import Editor from "react-simple-code-editor";
+import { highlight, languages } from "prismjs/components/prism-core";
+import "prismjs/components/prism-clike";
+import "prismjs/components/prism-javascript";
+import "prismjs/themes/prism.css";
+
+export default function App() {
+  
+  return (
+    <div>
+      this si test
+    </div>
+  );
+}
